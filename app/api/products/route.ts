0adb1ba2404c0d05fce6bfd4a31df576fe7db0d6
Felipe Rocha <felipemota.rocha@gmail.com>
2024-09-15 14:47:00 -0300

    refactor: add route handlers

---
+++ app/api/products/route.ts
@@ -0,0 +1,25 @@
+import { db } from "@/app/_lib/prisma";
+
+// Apenas para referência
+
+export async function GET() {
+  const products = await db.product.findMany({});
+  return Response.json(products, {
+    status: 200,
+  });
+}
+
+export async function POST(request: Request) {
+  const body = await request.json();
+  const name = body.name;
+  const price = body.price;
+  const stock = body.stock;
+  await db.product.create({
+    data: {
+      name,
+      price,
+      stock,
+    },
+  });
+  return Response.json({}, { status: 201 });
+}
