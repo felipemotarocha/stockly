070f7f524ff881a89e1bdfe18701a1954b3f6fc7
Felipe Rocha <felipemota.rocha@gmail.com>
2024-09-17 20:33:08 -0300

    feat: ensure sale action use next-safe-action

--- app/_actions/sale/create-sale/index.ts
+++ app/_actions/sale/create-sale/index.ts
@@ -1,50 +1,56 @@
 "use server";
 
 import { db } from "@/app/_lib/prisma";
-import { createSaleSchema, CreateSaleSchema } from "./schema";
+import { createSaleSchema } from "./schema";
 import { revalidatePath } from "next/cache";
+import { actionClient } from "@/app/_lib/safe-action";
+import { returnValidationErrors } from "next-safe-action";
 
-export const createSale = async (data: CreateSaleSchema) => {
-  createSaleSchema.parse(data);
-  await db.$transaction(async (trx) => {
-    const sale = await trx.sale.create({
-      data: {
-        date: new Date(),
-      },
-    });
-    for (const product of data.products) {
-      const productFromDb = await db.product.findUnique({
-        where: {
-          id: product.id,
+export const createSale = actionClient
+  .schema(createSaleSchema)
+  .action(async ({ parsedInput: { products } }) => {
+    await db.$transaction(async (trx) => {
+      const sale = await trx.sale.create({
+        data: {
+          date: new Date(),
         },
       });
-      if (!productFromDb) {
-        throw new Error("Product not found");
-      }
-      const productIsOutOfStock = product.quantity > productFromDb.stock;
-      if (productIsOutOfStock) {
-        throw new Error("Product out of stock");
-      }
-      await trx.saleProduct.create({
-        data: {
-          saleId: sale.id,
-          productId: product.id,
-          quantity: product.quantity,
-          unitPrice: productFromDb.price,
-        },
-      });
-      await trx.product.update({
-        where: {
-          id: product.id,
-        },
-        data: {
-          stock: {
-            decrement: product.quantity,
+      for (const product of products) {
+        const productFromDb = await db.product.findUnique({
+          where: {
+            id: product.id,
           },
-        },
-      });
-    }
+        });
+        if (!productFromDb) {
+          returnValidationErrors(createSaleSchema, {
+            _errors: ["Product not found."],
+          });
+        }
+        const productIsOutOfStock = product.quantity > productFromDb.stock;
+        if (productIsOutOfStock) {
+          returnValidationErrors(createSaleSchema, {
+            _errors: ["Product out of stock."],
+          });
+        }
+        await trx.saleProduct.create({
+          data: {
+            saleId: sale.id,
+            productId: product.id,
+            quantity: product.quantity,
+            unitPrice: productFromDb.price,
+          },
+        });
+        await trx.product.update({
+          where: {
+            id: product.id,
+          },
+          data: {
+            stock: {
+              decrement: product.quantity,
+            },
+          },
+        });
+      }
+    });
+    revalidatePath("/products");
   });
-
-  revalidatePath("/products");
-};
--- app/sales/_components/upsert-sheet-content.tsx
+++ app/sales/_components/upsert-sheet-content.tsx
@@ -38,6 +38,8 @@ import { z } from "zod";
 import SalesTableDropdownMenu from "./table-dropdown-menu";
 import { createSale } from "@/app/_actions/sale/create-sale";
 import { toast } from "sonner";
+import { useAction } from "next-safe-action/hooks";
+import { flattenValidationErrors } from "next-safe-action";
 
 const formSchema = z.object({
   productId: z.string().uuid({
@@ -69,6 +71,17 @@ const UpsertSheetContent = ({
   const [selectedProducts, setSelectedProducts] = useState<SelectedProduct[]>(
     [],
   );
+  const { execute: executeCreateSale } = useAction(createSale, {
+    onError: ({ error: { validationErrors, serverError } }) => {
+      const flattenedErrors = flattenValidationErrors(validationErrors);
+      toast.error(serverError ?? flattenedErrors.formErrors[0]);
+    },
+    onSuccess: () => {
+      toast.success("Venda realizada com sucesso.");
+      setSheetIsOpen(false);
+    },
+  });
+
   const form = useForm<FormSchema>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -135,18 +148,12 @@ const UpsertSheetContent = ({
     });
   };
   const onSubmitSale = async () => {
-    try {
-      await createSale({
-        products: selectedProducts.map((product) => ({
-          id: product.id,
-          quantity: product.quantity,
-        })),
-      });
-      toast.success("Venda realizada com sucesso!");
-      setSheetIsOpen(false);
-    } catch (error) {
-      toast.error("Erro ao realizar a venda.");
-    }
+    executeCreateSale({
+      products: selectedProducts.map((product) => ({
+        id: product.id,
+        quantity: product.quantity,
+      })),
+    });
   };
   return (
     <SheetContent className="!max-w-[700px]">
