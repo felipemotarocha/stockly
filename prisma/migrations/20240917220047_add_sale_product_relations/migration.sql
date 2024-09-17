503ff6a945caa47ef81e71c216c49586d3b3e68b
Felipe Rocha <felipemota.rocha@gmail.com>
2024-09-17 19:10:10 -0300

    chore: add foreign keys to sale and product table

---
+++ prisma/migrations/20240917220047_add_sale_product_relations/migration.sql
@@ -0,0 +1,5 @@
+-- AddForeignKey
+ALTER TABLE "SaleProduct" ADD CONSTRAINT "SaleProduct_saleId_fkey" FOREIGN KEY ("saleId") REFERENCES "Sale"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "SaleProduct" ADD CONSTRAINT "SaleProduct_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
