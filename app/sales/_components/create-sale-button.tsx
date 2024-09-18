374c30e99cc457f5d1f511ebc928429763cae05e
Felipe Rocha <felipemota.rocha@gmail.com>
2024-09-17 21:30:41 -0300

    feat: add plus icon to create sale button

--- app/sales/_components/create-sale-button.tsx
+++ app/sales/_components/create-sale-button.tsx
@@ -6,6 +6,7 @@ import UpsertSheetContent from "./upsert-sheet-content";
 import { Product } from "@prisma/client";
 import { ComboboxOption } from "@/app/_components/ui/combobox";
 import { useState } from "react";
+import { PlusIcon } from "lucide-react";
 
 interface CreateSaleButtonProps {
   products: Product[];
@@ -17,7 +18,10 @@ const CreateSaleButton = (props: CreateSaleButtonProps) => {
   return (
     <Sheet open={sheetIsOpen} onOpenChange={setSheetIsOpen}>
       <SheetTrigger asChild>
-        <Button>Nova Venda</Button>
+        <Button className="gap-2">
+          <PlusIcon size={20} />
+          Nova Venda
+        </Button>
       </SheetTrigger>
       <UpsertSheetContent setSheetIsOpen={setSheetIsOpen} {...props} />
     </Sheet>
