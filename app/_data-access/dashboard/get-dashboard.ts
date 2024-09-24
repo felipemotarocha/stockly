23e9e608e78744c9f85e82e7949415e6c8520f1a
Felipe Rocha <felipemota.rocha@gmail.com>
2024-09-24 19:41:10 -0300

    feat: ensure revenue is fetched by sale date

--- app/_data-access/dashboard/get-dashboard.ts
+++ app/_data-access/dashboard/get-dashboard.ts
@@ -29,9 +29,10 @@ export const getDashboard = async (): Promise<DashboardDto> => {
       { totalRevenue: number }[]
     >(
       `
-      SELECT SUM("unitPrice" * "quantity") as "totalRevenue"
-      FROM "SaleProduct"
-      WHERE "createdAt" >= $1 AND "createdAt" <= $2;
+    SELECT SUM("SaleProduct"."unitPrice" * "SaleProduct"."quantity") as "totalRevenue"
+    FROM "SaleProduct"
+    JOIN "Sale" ON "SaleProduct"."saleId" = "Sale"."id"
+    WHERE "Sale"."date" >= $1 AND "Sale"."date" <= $2;
     `,
       day.startOf("day").toDate(),
       day.endOf("day").toDate(),
@@ -42,14 +43,16 @@ export const getDashboard = async (): Promise<DashboardDto> => {
     });
   }
   const totalRevenueQuery = `
-    SELECT SUM("unitPrice" * "quantity") as "totalRevenue"
-    FROM "SaleProduct";
-  `;
+  SELECT SUM("SaleProduct"."unitPrice" * "SaleProduct"."quantity") as "totalRevenue"
+  FROM "SaleProduct"
+  JOIN "Sale" ON "SaleProduct"."saleId" = "Sale"."id";
+`;
   const todayRevenueQuery = `
-    SELECT SUM("unitPrice" * "quantity") as "todayRevenue"
-    FROM "SaleProduct"
-    WHERE "createdAt" >= $1 AND "createdAt" <= $2;
-  `;
+  SELECT SUM("SaleProduct"."unitPrice" * "SaleProduct"."quantity") as "todayRevenue"
+  FROM "SaleProduct"
+  JOIN "Sale" ON "SaleProduct"."saleId" = "Sale"."id"
+  WHERE "Sale"."date" >= $1 AND "Sale"."date" <= $2;
+`;
   const startOfDay = new Date(new Date().setHours(0, 0, 0, 0));
   const endOfDay = new Date(new Date().setHours(23, 59, 59, 999));
   const totalRevenuePromise =
